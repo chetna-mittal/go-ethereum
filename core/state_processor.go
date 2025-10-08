9393ef58a8dc1130cc596960b5e3e595da93fa99
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2025-10-08 12:05:43 +0200

    fix rebase issues

--- core/state_processor.go
+++ core/state_processor.go
@@ -91,10 +91,10 @@ func (p *StateProcessor) Process(ctx context.Context, block *types.Block, stated
 	// Apply pre-execution system calls.
 	context = NewEVMBlockContext(header, p.chain, nil)
 	evm := vm.NewEVM(context, tracingStateDB, config, cfg)
-	b, ok := p.chain.engine.(*beacon.Beacon)
+	b, ok := p.chain.Engine().(*beacon.Beacon)
 	if ok {
 		// XXX check this is ok
-		b.SetAuraSyscall(MakeAuraSyscall(tracingStateDB, context, p.chain.config, cfg))
+		b.SetAuraSyscall(MakeAuraSyscall(tracingStateDB, context, config, cfg))
 	}
 	b.AuraPrepare(p.chain, block.Header(), statedb)
 	if beaconRoot := block.BeaconRoot(); beaconRoot != nil {
@@ -116,7 +116,7 @@ func (p *StateProcessor) Process(ctx context.Context, block *types.Block, stated
 			telemetry.Int64Attribute("tx.index", int64(i)),
 		)
 
-		receipt, err := ApplyTransactionWithEVM(msg, gp, statedb, blockNumber, blockHash, context.Time, tx, evm)
+		receipt, err := ApplyTransactionWithEVM(msg, gp, statedb, blockNumber, blockHash, context.Time, tx, evm, p.chain.Engine())
 		if err != nil {
 			spanEnd(&err)
 			return nil, fmt.Errorf("could not apply tx %d [%v]: %w", i, tx.Hash().Hex(), err)
